0711ed5bc00d99c19afe435bbf594abdf04f4874
Artur <alimayko91@gmail.com>
2024-08-07 11:22:30 +0300

    lesson 4 repeated props

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,36 @@
 import React from 'react';
+import {Rating} from './Rating';
+import {Accordion} from "./Accordion";
+
 
 function App() {
     return (
         <div className="App">
-            This App Component
-            <Rating/>
-            <Accordion/>
+            <AppTitle title='Article 1'/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+
+            <AppTitle title='Article 2'/>
+            <Accordion title='Accordion Title'/>
         </div>
     );
 }
 
-function Rating() {
+function AppTitle(props: any) {
     return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-
+        <h1>{props.title}</h1>
     )
 }
 
-function Star() {
-    return (
-        <div>Star</div>
-    )
-}
-
-function Accordion() {
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    )
-}
-
-function AccordionTitle() {
-    return (
-        <h3>Меню</h3>
-    )
-}
-
-function AccordionBody() {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    )
-}
 
 export default App;
+
+// создать отдельные окмпоненты для рейтинга и аккордиона и тайтла( его оставим тут же просто создадим отдельную компоненту)
+// внутрь star  передать пропсы и создать условие если true  то выделен если нет то не выделен
+// в рейтинг тоже передадим пропсы и пропишем условие - если 1 2 3 4 5 то соответствующее количество звезд - иначе возвращаем false
+// Rating  в App передадим несколько значений - например value - 3, value 5
+// В аккордион прокинуть тайтл
