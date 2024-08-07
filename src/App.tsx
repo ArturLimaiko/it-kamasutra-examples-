fdde56339cdbd634cec539c6b64a9f0fcd7bcd5b
Artur <alimayko91@gmail.com>
2024-08-07 08:10:09 +0300

    lesson 3 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,57 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-        This App Component
-        <Rating/>
-        <Accordion/>
-    </div>
-  );
+    return (
+        <div className="App">
+            This App Component
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
 }
 
 function Rating() {
-  return (
-      <div>
-          <Star/>
-          <Star/>
-          <Star/>
-          <Star/>
-          <Star/>
-      </div>
-
-  )
-}
-
-function Star () {
-  return (
-      <div>Star</div>
-  )
-}
-
-function Accordion () {
     return (
         <div>
-            <h3>Меню</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+
+    )
+}
+
+function Star() {
+    return (
+        <div>Star</div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <Accordion/>
+            <AccordionBody/>
         </div>
     )
 }
 
+function AccordionTitle() {
+    return (
+        <h3>Меню</h3>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
 
 export default App;
