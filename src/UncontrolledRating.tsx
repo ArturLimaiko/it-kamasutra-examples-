04fd11b2d7e3f6aeb49e70b587b76999bd5e196a
Artur <alimayko91@gmail.com>
2024-08-20 22:22:34 +0300

    пркинул в неконтролируемый рейтинг колбек

--- src/UncontrolledRating.tsx
+++ src/UncontrolledRating.tsx
@@ -1,27 +1,28 @@
 import React, {useState} from "react";
 
-export type UncontrolledRatingType  = {
-     // value: number
-}
+export type UncontrolledRatingType = {}
 
 export function UncontrolledRating() {
-    const [value, setValue] = useState<UncontrolledRatingType>(2)
+    const [value, setValue] = useState<UncontrolledRatingType>(0)
 
     return (
         <div>
-            <Star selected={value > 0}/><button  onClick={()=> setValue(1)}>1</button>
-            <Star selected={value > 1}/><button  onClick={()=> setValue(2)}>2</button>
-            <Star selected={value > 2}/><button  onClick={()=> setValue(3)}>3</button>
-            <Star selected={value > 3}/><button  onClick={()=> setValue(4)}>4</button>
-            <Star selected={value > 4}/><button onClick={()=> setValue(5)}>5</button>
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
         </div>
     );
 }
 
 type StarType = {
     selected: boolean
+    value: 1 | 2 | 3 | 4 | 5
+    setValue: (value: 1 | 2 | 3 | 4 | 5) => void
 }
 
-export function Star({selected}: StarType) {
-    return selected ? <span><b> Star </b></span> : <span> Star </span>
+export function Star({selected, setValue, value}: StarType) {
+    return <span style={{cursor: "pointer", display: 'flex', justifyContent: 'center'}}
+                 onClick={() => setValue(value)}> {selected ? <b> Star </b> : 'star'}</span>
 }
