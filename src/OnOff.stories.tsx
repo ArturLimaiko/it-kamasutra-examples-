b4cdd1b107c3e18c90dcb7e9a90c5082a4affde7
Artur <alimayko91@gmail.com>
2024-09-08 12:30:06 +0300

    lesson 2 storybook

---
+++ src/OnOff.stories.tsx
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import {action} from "@storybook/addon-actions";
+import {OnOff} from "./OnOff";
+
+export default {
+    title: "OnOff stories",
+    component: OnOff,
+}
+
+const callBack = action('on or off clicked')
+
+
+export const OnMode = () => <OnOff switchOn={true} onChange={callBack}/>
+export const OffMode = () => <OnOff switchOn={false} onChange={callBack}/>
+export const ModeChanging = () => {
+    const [value, setValue] = useState<boolean>(true);
+   return <OnOff switchOn={value} onChange={setValue} />
+}
