b8353f69ef2bf6a6d90f4d7c92fff6beab6fd480
Artur <alimayko91@gmail.com>
2024-09-13 21:47:13 +0300

    13 react кабзда как подробно - про controlled input,checkbox,select

--- src/UncontrolledInput.stories.tsx
+++ src/UncontrolledInput.stories.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, useRef, useState} from "react";
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'Uncontrolled Input',
@@ -41,7 +42,67 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     )
 }
 
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('');
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChange}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true);
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+    return <input type={"checkbox"} checked={parentValue} onChange={onChange}/>
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>('2');
+
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChange}>
+        <option>none</option>
+        <option value={'1'}>Minsk</option>
+        <option value={'2'}>New York</option>
+        <option value={'3'}>Moscow</option>
+        <option value={'4'}>China</option>
+    </select>
+}
+
 export const ControlledInputWithFixedValue = () => <input value={'type here...'}/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
