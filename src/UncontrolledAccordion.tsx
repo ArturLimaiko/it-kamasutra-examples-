4a02db86cab7d92ef71dcced8ae8f21b15cfe0a8
Artur <alimayko91@gmail.com>
2024-09-21 21:30:47 +0300

    16 react кабзда как подробно - started useReducer

--- src/UncontrolledAccordion.tsx
+++ src/UncontrolledAccordion.tsx
@@ -1,15 +1,31 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
 
 type UncontrolledAccordionType = {
     titleValue: string;
 }
 
+type ActionType = {
+    type: string
+}
+
+const reducer = (state: boolean, action:ActionType) => {
+    //тут просто название инструкции - обязательно должно совпадать - если совпадает 'TOGGLE-COLLAPSED', то
+    if(action.type === 'TOGGLE-COLLAPSED'){
+        // то тогда верни противоположное значение state
+        return !state
+    }
+    return state
+}
+
 export function UncontrolledAccordion({titleValue}: UncontrolledAccordionType) {
-    const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, dispatch] = useReducer(reducer, false);
 
     return (
         <div>
-            <div ><AccordionTitle title={titleValue} onClick={()=> setCollapsed(!collapsed)}/>
+            <div >
+                {/*<AccordionTitle title={titleValue} onClick={()=> setCollapsed(!collapsed)}/>*/}
+                {/*в dispatch отправили экшн - при нажатии мы диспатчим инструкцию*/}
+                <AccordionTitle title={titleValue} onClick={()=> dispatch({type: 'TOGGLE-COLLAPSED'})}/>
                 {!collapsed && <div><AccordionBody/></div>}
             </div>
         </div>
