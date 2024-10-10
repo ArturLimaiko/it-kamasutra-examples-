62277b1f6fe1b38e8ede6973ef6f8e889da60833
Artur <alimayko91@gmail.com>
2024-10-10 13:56:24 +0300

    22 UseEffect setInterval & setTimeOut ( сделать часики формата 22: 13: 05 )

--- src/useEffect/useEffect.stories.tsx
+++ src/useEffect/useEffect.stories.tsx
@@ -20,7 +20,7 @@ export const SimpleExample = () => {
     useEffect(() => {
         console.log('useEffect only first render( component Didmount)')
         document.title = counter.toString()
-    },[])
+    }, [])
 
     //
     useEffect(() => {
@@ -32,6 +32,58 @@ export const SimpleExample = () => {
         Hello, {counter} , {fake}
         <button onClick={() => setFake(fake + 1)}>fake+</button>
         <button onClick={() => setCounter(counter + 1)}>counter+</button>
+    </>
+}
+
+export const SetTimeoutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log("SetTimeoutExample");
+
+    //все ассинхронные операции должны делать внутри useEffect()
+    useEffect(() => {
+
+    //когда меняется counter говорим перезапускай этот эффект (setTimeout) через секунду поменяется
+        setTimeout(() => {
+            console.log("setTimeout");
+            document.title = counter.toString()
+        }, 1000)
+
+    }, [counter]);
+
+    return <>
+        Hello, {counter} , {fake}
+        <button onClick={() => setFake(fake + 1)}>fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>
+    </>
+}
+
+export const SetIntervalExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log("SetIntervalExample");
+
+    useEffect(() => {
+
+        //тут вообще функция даже не вызывается целиком , setInterval запустился единожды
+        // а setCounter берет из замыкания тот  counter который был в самый 1 раз - а там была 1
+        //и он работает с 1 вызовом функции SetIntervalExample(), в counter сидит 1
+        // и при каждом ТИКЕ берет 1 , а 1 + 1 =2 и отправляет в  state , далее в counter типа 2 он отрисовывает единожды
+        // и useEffect больше не регистрируется тк зависимостей нет, но в counter была единичка и он забирает ее и сетает 2
+        // соответственно и перерисовка не происходит и setInterval берет значение из замыкания , а ему нужно свежее значение -
+        // а мы можем передать преобразователь которая говорит не конкретное значение на тебе а КАК преобразовать его!
+        //таким образом мы увидим  изменения
+        setInterval(() => {
+            console.log(" Tik: " + counter);
+            setCounter(state => state + 1)
+        }, 1000)
+
+    }, []);
+
+    return <>
+        Hello, counter: {counter} - fake: {fake}
     </>
 }
 
